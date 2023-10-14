bae598f0e672c5045873de3828597084f6dd87df
Jaralva <zaingrazes@mail.ru>
2023-10-14 15:46:10 +0300

    Open Ai

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
-KEY = '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'
+KEY = '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'
 
 openai.api_key = KEY
 
@@ -10,9 +10,17 @@ def generate_response(text):
         prompt=text,  # текст который мы передаем GPT
         engine='text-davinci-003', # движок AI
         max_tokens=100, # макс. длина символов
-        tempature=0.7, # уровень креативности AI
+        temperature=0.7, # уровень креативности AI
         n=1, # количество ответов
         stop=None, # стоп-слово
         timeout=15 # время на ответ
-
     )
+
+
+    if response and response.choices:
+        return response.choices[0].text.strip()
+    else:
+        return None
+
+res = generate_response('Привет как у тебя дела, какая погода сейчас в Минске?')
+print(res)
